f6b09c14edd354c71e28e1884497374bd6dc33de
Gleethos <32813334+Gleethos@users.noreply.github.com>
2017-12-10 16:06:44 +0100

    MapCreator updated, & BaseManager added

---
+++ Game/Assets/Scripts/BaseManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseManager : MonoBehaviour {
+
+
+	[SerializeField] 
+	private GameObject BaseTexture;
+
+
+	double Health;
+
+	int x;
+	int y;
+
+	// Use this for initialization
+	void Start () {x=0; y=0; Health=100;
+
+		GameObject newBase = Instantiate(BaseTexture);
+		newBase.transform.position = new Vector3(x, y, -3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void damageBase(double damage)
+	{Health -= damage;}
+
+	public void modifyBaseHealth(double modification)
+	{Health += modification;}
+
+
+
+
+}
--- Game/Assets/Scripts/MapCreator.cs
+++ Game/Assets/Scripts/MapCreator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+//using System;
 
 public class MapCreator : MonoBehaviour {
 
@@ -11,14 +12,14 @@ public class MapCreator : MonoBehaviour {
 	private int testMap;
 
 	[SerializeField] 
-	private Map TileMap;
+	private MapManager Map;
 
 	[SerializeField] 
 	private GameObject[] Tile;
 	[SerializeField] 
 	private GameObject[] TileCorner;
 		
-	public int[,] testMap2
+	public int[,] testMap0
 	= new int[,]{
 		{0,0,0,0,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0},
 		{0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0},
@@ -41,8 +42,9 @@ public class MapCreator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-		createLevel (TileMap.testMapArray);
+	Map = new MapManager();
+	
+	createLevel (Map.testMap);
 
 
 
@@ -53,22 +55,50 @@ public class MapCreator : MonoBehaviour {
 		
 	}
 
+	private void createLevel(MapManager Map)
+	{
+
+
+	}
+
+	private void createLevel(int[] map){
+	float l = map.Length;double a = Mathf.Pow (l, (1/2));  
+	int[,] mapTwoD = new int[(int)a, (int)a];
+	int counter = 0; 
+	for (int y = 0; y < a; y++) {
+		 for (int x = 0; x < a; x++) 
+			{
+				mapTwoD [y, x] = map [counter];
+			}counter++;}
+	createLevel (mapTwoD, a);
+	}
+
 	private void createLevel(int[,] map){
+	float l = map.Length;double a = Mathf.Pow (l, (1/2)); 
+		print ("HEEEEEEEY: "+l);
+	createLevel (map, a);
+	}
+
+
+	private void createLevel(int[,] map, double a){
+
+
 
 	float tileSize=Tile[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-	int length = 15;
-		int counter = 0;;
+	double length = 40;
+		int shift =(int)length/2;
+
 	for(int y = 0; y < length; y++)
 		{for(int x = 0; x < length; x++){
 				int rotationID = Random.Range(0, 4);
-				if(rotationID==0){placeTileAt(x, y, map[y,x], 0, 0);}
-				if(rotationID==1){placeTileAt(x, y, map[y,x], 0, 90);}
-				if(rotationID==2){placeTileAt(x, y, map[y,x], 0, 180);}
-				if(rotationID==3){placeTileAt(x, y, map[y,x], 0, 270);}
+				if(rotationID==0){placeTileAt(x-shift, y-shift, map[y,x], 0, 0);}
+				if(rotationID==1){placeTileAt(x-shift, y-shift, map[y,x], 0, 90);}
+				if(rotationID==2){placeTileAt(x-shift, y-shift, map[y,x], 0, 180);}
+				if(rotationID==3){placeTileAt(x-shift, y-shift, map[y,x], 0, 270);}
 				//placeTileAt(x, y, testMap2[y,x]);
-				smoothAt (x, y, map);
+				smoothAt (x, y, -shift, -shift, map);
 				//placeTileAt(x, y, 0, -1, 45);
-				counter++;
+
 			}
 
 		}
@@ -97,37 +127,46 @@ public class MapCreator : MonoBehaviour {
 	}
 		//Instantiate(grassTile);
 
-	private void smoothAt(int x, int y, int[,] map)
+	private void smoothAt(int x, int y, int xShift, int yShift, int[,] map)
 	{
-		if(map[y+1,x+1]==map[y,x+1]&map[y+1,x+1]==map[y+1,x]&map[y+1,x+1]!=map[y,x])
-		{placeCornerTileAt (x, y, map [y + 1,x + 1], -1, 180);}
+		if (map [y + 1, x + 1] == map [y, x + 1] & map [y + 1, x + 1] == map [y + 1, x] & map [y + 1, x + 1] != map [y, x]) {
+			placeCornerTileAt (x+xShift, y+yShift, map [y + 1, x + 1], -1, 180);
+		} else {
+			if(map[y,x+1]==map[y+1,x]&map[y+1,x]!=map[y,x])
+			{placeCornerTileAt (x+xShift, y+yShift, map [y + 1,x], -1, 180);}	
+		}
 
-		if(map[y,x+1]==map[y+1,x]&map[y+1,x]!=map[y,x])
-		{placeCornerTileAt (x, y, map [y + 1,x], -1, 180);}
-
+	
 		if(x>0&&y>0){
-			if(map[y-1,x-1]==map[y,x-1]&map[y-1,x-1]==map[y-1,x]&map[y-1,x-1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y - 1,x - 1], -1, 0);}
-		
-			if(map[y,x-1]==map[y-1,x]&map[y,x-1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y,x - 1], -1, 0);}
+			if (map [y - 1, x - 1] == map [y, x - 1] & map [y - 1, x - 1] == map [y - 1, x] & map [y - 1, x - 1] != map [y, x]) {
+				placeCornerTileAt (x+xShift, y+yShift, map [y - 1, x - 1], -1, 0);
+			} else {
+				if(map[y,x-1]==map[y-1,x]&map[y,x-1]!=map[y,x])
+				{placeCornerTileAt (x+xShift, y+yShift, map [y,x - 1], -1, 0);}
+
+			}
 
 		
 			}
 		
 		if(y>0){
-			if(map[y-1,x+1]==map[y,x+1]&map[y-1,x+1]==map[y-1,x]&map[y-1,x+1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y - 1,x +1], -1, 270);}
-		
-			if(map[y,x+1]==map[y-1,x]&map[y,x+1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y - 1,x], -1, 270);}
+			if (map [y - 1, x + 1] == map [y, x + 1] & map [y - 1, x + 1] == map [y - 1, x] & map [y - 1, x + 1] != map [y, x]) {
+				placeCornerTileAt (x+xShift, y+yShift, map [y - 1, x + 1], -1, 270);
+			} 
+			else {
+				if(map[y,x+1]==map[y-1,x]&map[y,x+1]!=map[y,x])
+				{placeCornerTileAt (x+xShift, y+yShift, map [y - 1,x], -1, 270);}
+			}
+
 		}
 		if (x > 0) {
-			if(map[y+1,x-1]==map[y,x-1]&map[y+1,x-1]==map[y+1,x]&map[y+1,x-1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y + 1,x - 1], -1, 90);}
+			if (map [y + 1, x - 1] == map [y, x - 1] & map [y + 1, x - 1] == map [y + 1, x] & map [y + 1, x - 1] != map [y, x]) {
+				placeCornerTileAt (x+xShift, y+yShift, map [y + 1, x - 1], -1, 90);
+				}else {
+				if(map[y,x-1]==map[y+1,x]&map[y,x-1]!=map[y,x])
+				{placeCornerTileAt (x+xShift, y+yShift, map [y,x - 1], -1, 90);}
+				}
 		
-			if(map[y,x-1]==map[y+1,x]&map[y,x-1]!=map[y,x])
-			{placeCornerTileAt (x, y, map [y,x - 1], -1, 90);}
 		}
 
 
