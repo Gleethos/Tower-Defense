dc8785a0e574312ea80c6e5fc7b102959463e8a0
Oliiver <oliver40@gmx.at>
2018-01-05 22:06:10 +0100

    add enemymovment scripts without rotation

---
+++ Game/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    public GameObject testEnemyPrefab;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
