d59022c1f4b1de9186a33159b1d936be5d47311e
Gleethos <32813334+Gleethos@users.noreply.github.com>
2018-01-14 20:16:57 +0100

    interface and map update - part 2

---
+++ Game/Assets/Scripts/Turret_Behavior_PewPew.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Behavior_PewPew : MonoBehaviour {
+
+	public int visionRadius;
+	public float attackSpeed;
+	public int dmg;
+	public float rotateSpeed;
+	float time;
+	float targetAngle;
+	public GameObject bullet;
+
+	List<GameObject> targetInRange;
+	GameObject target;
+
+
+	// Use this for initialization
+	void Start () {
+
+		targetInRange = new List<GameObject> ();
+
+
+		CircleCollider2D collider = GetComponent <CircleCollider2D> ();
+		collider.radius = visionRadius;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		time += Time.deltaTime*1;
+
+		//Debug.Log (time);
+
+		if (targetInRange.Count > 0 && target != null) {
+			//rotate to target
+			//Degrees of rotation
+			float targetAngle = Mathf.Atan2 (
+				                    transform.position.y - target.transform.position.y,
+				                    transform.position.x - target.transform.position.x
+			                    ) * Mathf.Rad2Deg;
+
+			float nextAngle = Mathf.MoveTowardsAngle (transform.eulerAngles.z, targetAngle, Time.deltaTime * rotateSpeed);
+
+			transform.eulerAngles = new Vector3 (0f, 0f, nextAngle);
+
+		
+			if (IsNearTargetAngle (transform.eulerAngles.z, targetAngle, 5f)) {
+
+				// Make yure enought time has passed to fire the bullet
+				if (time >= attackSpeed) {
+
+					//Debug.Log ("FIRE!");
+					GameObject bulletClone = Instantiate (bullet, transform.position, Quaternion.identity);
+					Bullet bulletScript = bulletClone.GetComponent <Bullet> ();
+					bulletScript.SetVelocityVectors (
+						Mathf.Cos (targetAngle * Mathf.Deg2Rad) * -1f, 
+						Mathf.Sin (targetAngle * Mathf.Deg2Rad) * -1f,
+						20f);
+
+					time = 0f;
+
+				}
+			}
+		
+		
+		} else {
+				
+			float nextAngle = Mathf.MoveTowardsAngle (transform.eulerAngles.z, 0f, Time.deltaTime * rotateSpeed);
+			transform.eulerAngles = new Vector3 (0f, 0f, nextAngle);
+			
+		}
+
+
+	}
+
+	bool IsNearTargetAngle(float current, float target, float offset){
+		//Debug.Log ("Differenz between target and current:" + (target - current));
+		//Debug.Log ("Mathf.Abs:" + Mathf.Abs (target - current));
+
+		if ((target - current) <= offset)
+			return true;
+		return false;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+
+		if (targetInRange.Count == 0) {
+			target = collider.gameObject;
+		}
+
+		targetInRange.Add (collider.gameObject);
+
+	}
+
+
+	void OnTriggerExit2D(Collider2D collider){
+
+		targetInRange.Remove (collider.gameObject);
+
+		if (targetInRange.Count > 0) {
+			target = targetInRange [0];
+		} else {
+			target = null;
+
+			//targetAngle =  Mathf.Atan2 (0,0) * Mathf.Rad2Deg;
+			//float nextAngle = Mathf.MoveTowardsAngle (transform.eulerAngles.z, 180f, Time.deltaTime * rotateSpeed);
+
+
+
+
+		}
+
+	}
+
+}
---
+++ Game/Assets/Scripts/Turret_Placement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret_Placement : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] Turret;
+
+    GameObject dummyTurret;
+
+    bool itemAttached = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (itemAttached) { print("Item attached to mouse coursor! "); checkInput(); }
+
+    }
+
+
+    private void checkInput() {
+        if (Input.GetMouseButton(0))
+        { itemAttached = false; print("Item deattached!"); }
+        else {dummyTurret.transform.position = new Vector3((Input.mousePosition.x-590)/25, (Input.mousePosition.y-475)/25, 2);
+            print("Mouse position X:" + Input.mousePosition.x + "; Mouse position Y:" + Input.mousePosition.y + "; ");
+        }
+        
+    }
+		 
+
+    public void getTurret(int towerIndex)
+        {
+        print("Mouse position X:"+Input.mousePosition.x+"; Mouse position Y:"+ Input.mousePosition.y+"; ");
+        dummyTurret = Instantiate(Turret[towerIndex]);
+        dummyTurret.transform.position = new Vector3((Input.mousePosition.x-590)/25, (Input.mousePosition.y-475)/25, 2);
+        itemAttached = true;
+        }
+
+
+    }
+
+
+
+
+
