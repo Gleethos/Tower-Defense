8ce2a288a92f6f151c9919a75518cfab531efa9c
Sonatai <gebauerjenny@yahoo.de>
2018-01-14 22:47:15 +0100

    Add files via upload

---
+++ Game/Assets/Prefabs/Scripts/Turret/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	float desapwnTime = 2;
+	//int Health = FindObjectOfType; 
+
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void SetVelocityVectors (float x, float y, float magnitude){
+
+		//how fast are u
+		velocity = new Vector3 (x* (Time.deltaTime / 2f), y*(Time.deltaTime / 2f), 0f) * magnitude; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//position update
+		transform.Translate (velocity);
+
+		//Despawning behavior
+		desapwnTime -= 1 * Time.deltaTime;
+
+		if (desapwnTime <= 0) {
+			Destroy (gameObject);
+		}
+	
+
+
+
+	}
+
+	public void OnCollisionEnter2D(Collision2D collider){
+		Destroy (this.gameObject);
+
+	}
+
+
+
+}
---
+++ Game/Assets/Prefabs/Scripts/Turret/Stampfi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stampfi: MonoBehaviour {
+	public float desapwnTime = 0.5f;
+	//int Health = FindObjectOfType; 
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+		
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		//Despawn time
+		desapwnTime -= 1 * Time.deltaTime;
+
+		if (desapwnTime <= 0) {
+			Destroy (gameObject);
+		}
+
+
+
+
+	}
+
+
+
+
+
+}
